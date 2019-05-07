71118444f506b10172db762de839ae2ec9303eea
刘进行 <jackingstarliu@gmail.com>
2019-05-07 14:43:43 +0800

    chore: 添加README文档

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,15 +11,15 @@
 const siteConfig = {
   title: 'sinoui开发指南', // Title for your website.
   tagline: '基于React和sinoui的应用开发指南',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://sinoui.github.io', // Your website URL
+  baseUrl: '/sinoui-guide/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
   projectName: 'sinoui-guide',
-  organizationName: 'sinosoft',
+  organizationName: 'sinoui',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
