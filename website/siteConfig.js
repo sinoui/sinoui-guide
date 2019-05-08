48bb8fa0c21313c189952d39491ac4a373d19b12
刘进行 <jackingstarliu@gmail.com>
2019-05-08 18:06:32 +0800

    chore: 优化文档中的代码片段样式

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -9,36 +9,36 @@
 // site configuration options.
 
 const siteConfig = {
-  title: 'sinoui开发指南', // Title for your website.
-  tagline: '基于React和sinoui的应用开发指南',
-  url: 'https://sinoui.github.io', // Your website URL
-  baseUrl: '/sinoui-guide/', // Base URL for your project */
+  title: "sinoui开发指南", // Title for your website.
+  tagline: "基于React和sinoui的应用开发指南",
+  url: "https://sinoui.github.io", // Your website URL
+  baseUrl: "/sinoui-guide/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'sinoui-guide',
-  organizationName: 'sinoui',
+  projectName: "sinoui-guide",
+  organizationName: "sinoui",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'react-index', label: '基础知识'},
-    {doc: 'app-dev-index', label: '应用开发指南'},
+    { doc: "react-index", label: "基础知识" },
+    { doc: "app-dev-index", label: "应用开发指南" },
     {
-      href: 'https://github.com/sinoui/sinoui-guide',
-      label: 'GitHub',
+      href: "https://github.com/sinoui/sinoui-guide",
+      label: "GitHub"
     },
-    {page: 'help', label: 'Help'},
+    { page: "help", label: "Help" }
   ],
 
   /* Colors for website */
   colors: {
-    primaryColor: '#03a9f4',
-    secondaryColor: '#9c27b0',
+    primaryColor: "#03a9f4",
+    secondaryColor: "#9c27b0"
   },
 
   /* Custom fonts for website */
@@ -60,17 +60,26 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: "atom-one-dark"
   },
 
+  usePrism: ["jsx"],
+
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
+  onPageNav: "separate",
   // No .html extensions for paths.
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+  ogImage: "img/undraw_online.svg",
+  twitterImage: "img/undraw_tweetstorm.svg",
+
+  scripts: [
+    "https://buttons.github.io/buttons.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js",
+    "/sinoui-guide/js/code-block-buttons.js"
+  ],
+  stylesheets: ["/sinoui-guide/css/code-block-buttons.css"]
 
   // Show documentation's last contributor's name.
   // enableUpdateBy: true,
